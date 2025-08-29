7e79a2989699aa7d8bc420cca66e31c1479673c5
isitfall <pdand94@gmail.com>
2025-08-29 20:42:49 +0300

    updated imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import databaseConfig from './config/db-config';
+import databaseConfig from './config/db.config';
 
 @Module({
   imports: [
