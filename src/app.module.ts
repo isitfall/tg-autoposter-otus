7d6db9cc4b7e5e9e3fb83953b827ce5c1981c085
isitfall <pdand94@gmail.com>
2025-08-29 20:41:53 +0300

    init db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import databaseConfig from './config/db-config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig],
+    }),
+    TypeOrmModule.forRoot(databaseConfig() as TypeOrmModuleOptions),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
