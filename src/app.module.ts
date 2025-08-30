ff7d6275cb8568ebb61f0aa750f74e7214d50117
isitfall <pdand94@gmail.com>
2025-08-30 10:59:07 +0300

    Registered auth and users modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 import databaseConfig from './config/db.config';
 import authConfig from './config/auth.config';
 
@@ -13,6 +15,8 @@ import authConfig from './config/auth.config';
       load: [databaseConfig, authConfig],
     }),
     TypeOrmModule.forRoot(databaseConfig() as TypeOrmModuleOptions),
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
